4efbd2d7d5b7f9f50d8a72ee7c3360c17d0c235a
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-17 10:24:10 -0300

    ticket: paging

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -199,7 +199,6 @@ export default class MoviesDAO {
    * that would match this query
    */
   static async getMovies({
-    // here's where the default parameters are set for the getMovies method
     filters = null,
     page = 0,
     moviesPerPage = 20,
@@ -227,18 +226,7 @@ export default class MoviesDAO {
       return { moviesList: [], totalNumMovies: 0 }
     }
 
-    /**
-    Ticket: Paging
-
-    Before this method returns back to the API, use the "moviesPerPage" and
-    "page" arguments to decide the movies to display.
-
-    Paging can be implemented by using the skip() and limit() cursor methods.
-    */
-
-    // TODO Ticket: Paging
-    // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.skip(page*moviesPerPage).limit(moviesPerPage)
 
     try {
       const moviesList = await displayCursor.toArray()
